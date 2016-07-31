9e721cae0b8a52b5a2d2933e255cfc5cdd83ed75
Scott Duffy <duffyscottc@gmail.com>
2016-07-31 18:17:11 -0400

    Created a program to display a series of images in a UICollectionView.

--- CollectionViewTutorial/ViewController.swift
+++ CollectionViewTutorial/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    var tableData: [String] = ["Evo X", "458", "GTR"]
+    var tableImages: [String] = ["evox.jpg", "458.jpg", "gtr.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Test")
+        print("Testing")
+        
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tableData.count //Return the number of items in the tableData array (created above)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: colvwCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! colvwCell //Link it up to the custom class we made, colvwCell.swift
+        
+        cell.label.text = tableData[indexPath.row] //Update the text of the  "label" UILabelView (defined in colvwCell.swift)
+        cell.image.image = UIImage(named: tableImages[indexPath.row]) //Update the text of the "image" UIImageView (defined in colvwCell.swift) (in hindsight, I should have called it something other than "image" to be less confusing, but it's the middle one.)
+        
+        return cell
+        
+    }
+    
+    //Now here's a function that prints some info out to the console when the user touches the cell.
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Cell \(indexPath.row) selected") //Print which cell was selected
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ CollectionViewTutorial/colvwCell.swift
@@ -0,0 +1,18 @@
+//
+//  colvwCell.swift
+//  CollectionViewTutorial
+//
+//  Created by Scott on 7/31/16.
+//  Copyright © 2016 Brosnappedfink. All rights reserved.
+//
+
+import UIKit
+
+class colvwCell: UICollectionViewCell {
+    
+    @IBOutlet var image: UIImageView!
+    
+    @IBOutlet var label: UILabel!
+    
+    
+}
